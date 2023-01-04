7223d749dd1ec5c62cb15e3ffdb642b36c3ced46
Chirag Arora <arora2002chirag@gmail.com>
2023-01-05 05:16:51 +0530

    Initial Commit

---
+++ TeslaApp/TeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaAppApp.swift
+//  TeslaApp
+//
+//  Created by chirag arora on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
